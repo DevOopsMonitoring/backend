57922a0733acb16633fefe887c3d1d967147b536
w0rng <antonabramov2000@gmail.com>
2021-06-01 17:42:59 +0700

    fix url

--- src/config.py
+++ src/config.py
@@ -4,5 +4,7 @@ ENV = os.getenv("FLASK_ENV")
 DEBUG = ENV == "development"
 SECRET_KEY = os.getenv("SECRET_KEY")
 
-SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI")
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+    SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 SQLALCHEMY_TRACK_MODIFICATIONS = False
