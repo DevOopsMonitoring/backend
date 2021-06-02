dcc3d620cfbae973f15f29d14821af561b26ac1b
w0rng <antonabramov2000@gmail.com>
2021-06-02 12:57:33 +0700

    fix phone

--- src/models/company.py
+++ src/models/company.py
@@ -6,7 +6,7 @@ class Company(db.Model):
     name = db.Column(db.String(80), nullable=False)
     description = db.Column(db.String(512), nullable=True)
     address = db.Column(db.String(128), nullable=False)
-    phone = db.Column(db.String(12), nullable=False)
+    phone = db.Column(db.String(18), nullable=False)
     users = db.relationship('User', backref='company', lazy=True)
 
     def __repr__(self):
